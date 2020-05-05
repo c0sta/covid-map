322ebdd749073517f0d274da33471b28db876188
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-05 14:28:41 -0300

    :lipstick: Added translucent statusbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Map} from './components/Map/Map';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, StatusBar} from 'react-native';
 
 const App: React.FC = () => {
   return (
     <View style={styles.mapContainer}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="rgba(255, 255, 255, 0)"
+        translucent={true}
+      />
       <Map />
     </View>
   );
