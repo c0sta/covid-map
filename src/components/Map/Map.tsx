5e0322d1e444299985b24d520d7ce5e144e7eebc
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-14 20:59:47 -0300

    :ambulance: fixed the absurd lagging

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import MapView, {Marker} from 'react-native-maps';
-import {Text, Image} from 'react-native';
+import MapView from 'react-native-maps';
 import {getData} from '../../services/get-data';
+import {getCountryNameByLatlng} from './services/map-api';
 // Dark mode map styles
 import {mapStyle} from './Map.dark';
 import {styles} from './Map.style';
+import Pointer from '../Pointer/Pointer';
 export interface CountryInfo {
   _id: number;
   iso2: string;
@@ -33,6 +34,22 @@ export interface CountryI {
 interface MarkerState {
   countries: Array<CountryI>;
   tracksViewChanges: boolean;
+  currentLatLong: {latitude: Number; longitude: Number};
+  visible: Boolean;
+  currentCountry: {
+    id: Number;
+    name: String;
+    prov: String;
+    flag: String;
+    latLong: {
+      latitude: number;
+      longitude: number;
+    };
+    cases: Number;
+    recovered: Number;
+    deaths: Number;
+    tests: Number;
+  };
 }
 class Map extends React.Component<{}, MarkerState> {
   constructor(props: any) {
@@ -40,13 +57,29 @@ class Map extends React.Component<{}, MarkerState> {
     this.state = {
       countries: [],
       tracksViewChanges: true,
+      visible: false,
+      currentLatLong: {latitude: 0, longitude: 0},
+      currentCountry: {
+        id: 0,
+        name: '',
+        prov: '',
+        flag: '',
+        latLong: {
+          latitude: 0,
+          longitude: 0,
+        },
+        cases: 0,
+        recovered: 0,
+        deaths: 0,
+        tests: 0,
+      },
     };
   }
   componentDidMount() {
     getData('/countries')
       .then((response) => {
         this.setState({countries: response});
-        setTimeout(this.disableViewChangesTracker, 2000);
+        setTimeout(this.disableViewChangesTracker, 1500);
       })
       .catch((err) => console.log(err));
   }
@@ -55,6 +88,59 @@ class Map extends React.Component<{}, MarkerState> {
     return this.setState({tracksViewChanges: false});
   };
 
+  handlePressedRegion(event: any) {
+    this.setState({visible: !this.state.visible});
+
+    const {
+      coordinate: {latitude, longitude},
+    } = event;
+    const latLongObject = {
+      coordinate: {
+        latitude: latitude,
+        longitude: longitude,
+      },
+    };
+
+    this.setState({currentLatLong: {latitude, longitude}});
+
+    getCountryNameByLatlng(latLongObject)
+      .then((response: any) => {
+        const {data} = response;
+        /** Filters country bt ISO2, e.g: CA, BR, US */
+        const selectedCountry = this.state.countries.filter(
+          (country) => country.countryInfo.iso2 === data.prov,
+        );
+        const {
+          country,
+          countryInfo,
+          cases,
+          recovered,
+          deaths,
+          tests,
+          // testsPerOneMillion,
+        } = selectedCountry[0];
+
+        this.setState({
+          currentCountry: {
+            id: countryInfo._id,
+            name: country,
+            prov: data.prov,
+            flag: countryInfo.flag,
+            latLong: {
+              latitude: countryInfo.lat,
+              longitude: countryInfo.long,
+            },
+            cases: cases,
+            recovered: recovered,
+            deaths,
+            tests,
+          },
+        });
+        console.log(this.state.currentCountry);
+      })
+      .catch((error) => console.log(error));
+  }
+
   render() {
     const {countries, tracksViewChanges} = this.state;
     return (
@@ -62,34 +148,31 @@ class Map extends React.Component<{}, MarkerState> {
         customMapStyle={mapStyle}
         cacheEnabled={true}
         loadingEnabled={true}
-        style={styles.mapView}>
-        {countries.map((item: CountryI) => {
-          const {
-            countryInfo: {_id, lat, long},
-            cases,
-          } = item;
-
-          const latLng = {
-            latitude: lat,
-            longitude: long,
-          };
-          return (
-            <Marker
-              key={_id || Math.random()}
-              style={styles.markerContainer}
-              coordinate={latLng}
-              tracksViewChanges={tracksViewChanges}>
-              {/* <Image
-                style={styles.flagImg}
-                source={{
-                  uri: flag,
-                }}
-                onLoad={() => this.disableViewChangesTracker}
-              /> */}
-              <Text style={styles.markerTitle}>{cases}</Text>
-            </Marker>
-          );
-        })}
+        style={styles.mapView}
+        onMarkerPress={(e) => this.handlePressedRegion(e.nativeEvent)}>
+        {!countries
+          ? null
+          : countries.map((country) => {
+              const coordinate = {
+                latitude: country.countryInfo.lat,
+                longitude: country.countryInfo.long,
+              };
+              const {
+                countryInfo: {_id},
+              } = country;
+              if (_id !== null)
+                return (
+                  <Pointer
+                    key={_id}
+                    coordinate={coordinate}
+                    tracksViewChanges={tracksViewChanges}
+                    handleTracksViewChanges={() =>
+                      this.disableViewChangesTracker
+                    }
+                    countryData={country}
+                  />
+                );
+            })}
       </MapView>
     );
   }
