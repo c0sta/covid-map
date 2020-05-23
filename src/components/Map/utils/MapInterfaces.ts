6a8e90a85544f724f22a5c9914ffa19c5ffee7d9
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-23 18:05:11 -0300

    :art: isolated map interfaces

---
+++ src/components/Map/utils/MapInterfaces.ts
@@ -0,0 +1,46 @@
+export interface CountryInfo {
+  _id: number;
+  iso2: string;
+  iso3: string;
+  lat: number;
+  long: number;
+  flag: string;
+}
+export interface CountryI {
+  updated: number;
+  country: string;
+  continent: string;
+  cases: number;
+  deaths: number;
+  recovered: number;
+  tests: number;
+  countryInfo: CountryInfo;
+  todayCases: number;
+  todayDeaths: number;
+  active: number;
+  critical: number;
+  casesPerOneMillion: number;
+  deathsPerOneMilion: number;
+  testsPerOneMillion: number;
+}
+
+export interface MarkerState {
+  countries: Array<CountryI>;
+  tracksViewChanges: boolean;
+  currentLatLong: {latitude: number; longitude: number};
+  visible: Boolean;
+  currentCountry: {
+    id: number;
+    name: string;
+    prov: string;
+    flag: string;
+    latLong: {
+      latitude: number;
+      longitude: number;
+    };
+    cases: number;
+    recovered: number;
+    deaths: number;
+    tests: number;
+  };
+}
