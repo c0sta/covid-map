9468b43f3a357c6386c88e2da51130f6c71a0127
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-24 11:19:13 -0300

    :art: created object to deal w/ my colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  red: '#FC7350',
+  blue: '#4F65F7',
+  green: '#14E282',
+  white: '#F1F1F1',
+  lightGray: '#e1e1e1',
+  black: '#141414',
+  yellow: '#e8e817',
+  darkBlue: '#212a37',
+};
+
+export default colors;
