56e13dd996c5427343f4703ea6deda2c2a3833bb
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-24 11:20:26 -0300

    :art: add icon to tab navigator

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,22 +3,36 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Map from '../components/Map/Map';
 import CountryList from '../screens/CountryList/CountryList';
+import Entypo from 'react-native-vector-icons/Entypo';
+import colors from '../styles/colors';
 
 export default function Routes() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
       <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName: any;
+            if (route.name === 'World') {
+              iconName = focused ? 'globe' : '';
+            } else if (route.name === 'Countrys') {
+              iconName = focused ? '' : 'line-graph';
+            }
+
+            // You can return any component that you like here!
+            return <Entypo name={iconName} size={size} color={color} />;
+          },
+        })}
         tabBarOptions={{
-          activeBackgroundColor: '',
-          activeTintColor: '#f1f1f1',
-          inactiveTintColor: 'gray',
+          activeTintColor: colors.black,
+          inactiveTintColor: '#b3b3b3',
           style: {
-            backgroundColor: '#212a37',
+            backgroundColor: colors.white,
           },
         }}>
-        <Tab.Screen name="World Cases" component={Map} />
-        <Tab.Screen name="Country's" component={CountryList} />
+        <Tab.Screen name="World" component={Map} />
+        <Tab.Screen name="Countrys" component={CountryList} />
       </Tab.Navigator>
     </NavigationContainer>
   );
