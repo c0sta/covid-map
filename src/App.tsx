6fe30d71daac4ed0d232f38427babe5cb0b29823
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-25 00:46:50 -0300

    :fire: removed SatusBar from AppComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
 import Routes from './routes/routes';
 
 const App: React.FC = () => {
-  return (
-    <>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="rgba(255, 255, 255, 0)"
-        translucent={true}
-      />
-      <Routes />
-    </>
-  );
+  return <Routes />;
 };
 
 export default App;
