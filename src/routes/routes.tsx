708c5ae258bbf986fe772abeb288ed8191bec45e
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-25 00:50:46 -0300

    :art: Added StatusBar to routes file

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,38 +2,47 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Map from '../components/Map/Map';
-import CountryList from '../screens/CountryList/CountryList';
-import Entypo from 'react-native-vector-icons/Entypo';
+import DashboardPage from '../screens/dashboard/DashboardPage';
 import colors from '../styles/colors';
+import {StatusBar} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 export default function Routes() {
   const Tab = createBottomTabNavigator();
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName: any;
-            if (route.name === 'World') {
-              iconName = focused ? 'globe' : '';
-            } else if (route.name === 'Countrys') {
-              iconName = focused ? '' : 'line-graph';
-            }
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="rgba(255, 255, 255, 0)"
+        translucent={true}
+        showHideTransition="fade"
+      />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName: any;
+              if (route.name === 'World') {
+                iconName = 'globe';
+              } else if (route.name === 'Countrys') {
+                iconName = 'line-graph';
+              }
 
-            // You can return any component that you like here!
-            return <Entypo name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: colors.black,
-          inactiveTintColor: '#b3b3b3',
-          style: {
-            backgroundColor: colors.white,
-          },
-        }}>
-        <Tab.Screen name="World" component={Map} />
-        <Tab.Screen name="Countrys" component={CountryList} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              // You can return any component that you like here!
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: colors.text,
+            inactiveTintColor: 'lightgray',
+            style: {
+              // backgroundColor: colors.white,
+            },
+          }}>
+          <Tab.Screen name="World" component={Map} />
+          <Tab.Screen name="Countrys" component={DashboardPage} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
